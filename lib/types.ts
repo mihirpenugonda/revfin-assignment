eb964ff9062dcafaf4736c1a1c88502601f11cba
Mihir Penugonda <mihirpenugonda@gmail.com>
2024-08-05 21:19:40 +0530

    initialize project and setup basic page layout

---
+++ lib/types.ts
@@ -0,0 +1,86 @@
+export type Color = {
+  r: number;
+  g: number;
+  b: number;
+};
+
+export type Camera = {
+  x: number;
+  y: number;
+};
+
+export enum LayerType {
+  Text,
+  Note,
+  Rectangle,
+  Ellipse,
+  Path,
+}
+
+export type RectangleLayer = {
+  type: LayerType.Rectangle;
+  x: number;
+  y: number;
+  height: number;
+  width: number;
+  fill: Color;
+  value?: string;
+};
+
+export type Point = {
+  x: number;
+  y: number;
+};
+
+export type XYWH = {
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+};
+
+export enum Side {
+  Top = 1,
+  Bottom = 2,
+  Left = 4,
+  Right = 8,
+}
+
+export type CanvasState =
+  | {
+      mode: CanvasMode.None;
+    }
+  | {
+      mode: CanvasMode.Pressing;
+      origin: Point;
+    }
+  | {
+      mode: CanvasMode.SelectionNet;
+      origin: Point;
+      current?: Point;
+    }
+  | {
+      mode: CanvasMode.Translating;
+      current: Point;
+    }
+  | {
+      mode: CanvasMode.Inserting;
+      layerType: LayerType.Rectangle;
+    }
+  | {
+      mode: CanvasMode.Resizing;
+      initialBounds: XYWH;
+      corner: Side;
+    };
+
+export enum CanvasMode {
+  None,
+  Pressing,
+  SelectionNet,
+  Translating,
+  Inserting,
+  Resizing,
+  Pencil,
+}
+
+export type Layer = RectangleLayer;
---
+++ lib/utils.ts
@@ -0,0 +1,11 @@
+import { Camera } from "./types";
+
+export const pointerEventToCanvasPoint = (
+  e: React.PointerEvent,
+  camera: Camera
+) => {
+  return {
+    x: Math.round(e.clientX) - camera.x,
+    y: Math.round(e.clientY) - camera.y,
+  };
+};
